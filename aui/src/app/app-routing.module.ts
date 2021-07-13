d25c4ef1fa2858e69e115dc27f49bde06a0c6a90
Shaurya <sonu.hooda@gmail.com>
2021-07-13 01:05:30 +0000

    aui

---
+++ aui/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import  {ViewAssetComponent} from './view-asset/view-asset.component'
+import {AddAssetComponent} from './add-asset/add-asset.component'
+import { UpdateAssetComponent } from './update-asset/update-asset.component';
+import { UpdateGuard } from './shared/update.guard';
+
+
+/*
+  Add the route paths to navigate to appropriate
+  1. /addAsset -> AddAssetComponent
+  2. /viewAsset -> ViewAssetComponent
+  3. /updateAsset/:assetId -> UpdateAssetComponet; update route gaurd should be applied
+  4. In case an Invalid URL is provided, the user must be redirected to the addAsset page.
+  */
+
+export const routes: Routes = [
+  
+    {path: '', component: AddAssetComponent},
+    {path: 'viewAsset', component: ViewAssetComponent},
+    {path: 'updateAsset/:Id', canActivate:[UpdateGuard], component: UpdateAssetComponent}
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ aui/src/app/shared/Asset.ts
@@ -0,0 +1,8 @@
+export default class Asset {
+  assetId: number;
+  assetName: string;
+  assetCategory: string;
+  assetDescription: string;
+  dateOfPurchase: string;
+  assetCost: number;
+}
